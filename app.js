eb9483431b0787789fad9fb03f37f2f0a00e0ccb
İbrahim Koçal <ibrahimkocal@gmail.com>
2024-11-16 20:02:26 +0300

    async ile userId ile veri çekimi

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("merhsssaba");
+async function getData(userId) {
+    const result = await(
+        await fetch (`https://jsonplaceholder.typicode.com/users/` + 4)
+    ).json();
+
+    console.log(result);
+}
+
+getData(2);
