3c830fa4415bacf65b3abc0bfebed082f3f834b8
İbrahim Koçal <ibrahimkocal@gmail.com>
2024-11-16 20:12:51 +0300

    default

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 async function getData(userId) {
     const result = await(
-        await fetch (`https://jsonplaceholder.typicode.com/users/` + 4)
+        await fetch (`https://jsonplaceholder.typicode.com/users/` + userId)
+    ).json();
+
+    const result2 = await(
+        await fetch (`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
     ).json();
 
     console.log(result);
+    console.log(result2);
+
 }
 
 getData(2);
